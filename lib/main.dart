cefa06eb1237b572160d7b28cfee7877d710b90b
FlutLab.io <>
2022-05-03 16:17:14 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rodriguez/prepaguerreros.dart';
 
 void main() {
   runApp(PrepaGuerrerosApp());
@@ -16,47 +17,3 @@ class PrepaGuerrerosApp extends StatelessWidget {
         home: PaginaPrincipal());
   } //widget
 } //clase prepaguerreros widget sin estado
-
-class PaginaPrincipal extends StatefulWidget {
-  PaginaPrincipal({Key? key}) : super(key: key);
-  @override
-  _PaginaPrincipalState createState() => _PaginaPrincipalState();
-} //widget con estado
-
-class _PaginaPrincipalState extends State<PaginaPrincipal> {
-  List<String> images = [
-    "assets/images/logo3.jpg",
-    "assets/images/logo2.jpg",
-    "assets/images/logo4.jpeg",
-    "assets/images/logo1.jfif",
-    "assets/images/logo5.jpg",
-    "assets/images/logo6.jpg",
-    "assets/images/logo7.jpg",
-    "assets/images/logo8.jpg",
-    "assets/images/logo8.jpg",
-    "assets/images/logo1.jpg",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Preparatorias Guerreros"),
-        ),
-        body: GridView.custom(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          childrenDelegate: SliverChildBuilderDelegate(
-            (BuildContext, index) {
-              return Image.asset(
-                images[index],
-                fit: BoxFit.cover,
-              );
-            },
-            childCount: 10,
-          ),
-          padding: EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
-  }
-}
